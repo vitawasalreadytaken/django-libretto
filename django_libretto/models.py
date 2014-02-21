ffb32643b01a948f1d055217aae949438ca1c155
Vita Smid <me@ze.phyr.us>
2014-02-21 09:40:54 +0100

    extendManager documentation

--- django_libretto/models.py
+++ django_libretto/models.py
@@ -4,11 +4,27 @@
 # Django imports.
 from django.db import models
 
-# Project/app imports.
-#
 
 
 def extendManager(mixinClass):
+	'''
+	Use as a class decorator to add extra methods to your model manager.
+	Example usage:
+
+		class Article(django.db.models.Model):
+			published = models.DateTimeField()
+			...
+
+			@extendManager
+			class objects(object):
+				def getPublished(self):
+					return self.filter(published__lte = django.utils.timezone.now()).order_by('-published')
+
+		...
+
+		publishedArticles = Article.objects.getPublished()
+	'''
+
 	class MixinManager(models.Manager, mixinClass):
 		class MixinQuerySet(models.query.QuerySet, mixinClass):
 			pass
