ca132eb0389bff7e1e155a12fa020ea92486665f
Vita Smid <me@ze.phyr.us>
2014-12-13 17:24:05 +0100

    Added django_libretto.templatetags to installed packages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	version = '0.1.0',
 	author = 'Vita Smid',
 	author_email = 'me@ze.phyr.us',
-	packages = ['django_libretto',],
+	packages = ['django_libretto', 'django_libretto.templatetags',],
 	url = 'https://github.com/ze-phyr-us/django-libretto',
 	license = 'LICENSE',
 	description = 'Symlink packages from your virtualenv so that GAE can find and deploy them.'
