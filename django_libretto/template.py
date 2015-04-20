c6d8f5e87e473de5bc2a809bf8d5aa425e943a20
Vita Smid <me@ze.phyr.us>
2015-04-20 18:23:54 +0800

    Added template.renderString

--- django_libretto/template.py
+++ django_libretto/template.py
@@ -5,3 +5,9 @@ import django.template
 def renderFile(path, context):
 	t = django.template.loader.get_template(path)
 	return t.render(django.template.Context(context))
+
+
+
+def renderString(template, context):
+	t = django.template.Template(template)
+	return t.render(django.template.Context(context))
