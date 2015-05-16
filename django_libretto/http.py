e82ce73e729c94d964468015b22a55c16a52fc95
Vita Smid <me@ze.phyr.us>
2015-05-16 19:11:20 +0800

    Removed mimetype argument from JsonResponse (removed in Django 1.7).

--- django_libretto/http.py
+++ django_libretto/http.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 
 
 class JsonResponse(HttpResponse):
-	def __init__(self, content={}, mimetype=None, status=None, content_type=None):
+	def __init__(self, content={}, status=None, content_type=None):
 		if not content_type:
 			content_type = 'application/json'
-		super(JsonResponse, self).__init__(json.dumps(content), mimetype=mimetype, status=status, content_type=content_type)
+		super(JsonResponse, self).__init__(json.dumps(content), status=status, content_type=content_type)
