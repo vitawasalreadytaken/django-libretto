d452c2d8b8a5251014bd554be1b524afa739c8d8
Vita Smid <me@ze.phyr.us>
2016-01-26 17:18:11 +0800

    Proper PyPI release

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
 	name = 'django-libretto',
-	version = '0.1.0',
+	version = '1.0.0',
 	author = 'Vita Smid',
 	author_email = 'me@ze.phyr.us',
 	packages = ['django_libretto', 'django_libretto.templatetags',],
 	url = 'https://github.com/ze-phyr-us/django-libretto',
-	license = 'LICENSE',
-	description = 'Collection of useful Django snippets.'
+	license = 'MIT',
+	description = 'Collection of useful Django snippets'
 )
